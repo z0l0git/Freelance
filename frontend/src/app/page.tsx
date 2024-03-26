ff6d18f9de39474e27a0a19df3b20764f49a9a5c
Izanagishdee <161716270+Izanagishdee@users.noreply.github.com>
2024-03-26 13:08:35 +0800

    testheader

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is the home page.</p>
+    </div>
   );
 }
