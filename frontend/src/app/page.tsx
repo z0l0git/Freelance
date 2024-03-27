711dba1a6341efacc0bce2286d55fd48f1de17ee
Sainjargal Gankhuyag <23LP8204@G302-CC0089.local>
2024-03-27 08:31:44 +0800

    27272

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
-import { HeaderHero } from "@/components/HeaderHero";
+
 export default function Home() {
-  return (
-    <div>
-      <HeaderHero />
-    </div>
-  );
+  return <div></div>;
 }
