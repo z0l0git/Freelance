54089c221bda802291fd92cc7bea431533fee4f4
Zolbayar Batbuyan <23LP5833@G302-CC0093.local>
2024-03-27 13:52:53 +0800

    menu

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HeaderHero } from "@/components/HeaderHero";
 import { Header } from "@/components/Header/HeaderComp";
+import { Menu } from "@/components/Menu";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Menu />
       <HeaderHero />
     </div>
   );
