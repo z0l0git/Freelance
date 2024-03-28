71c8cc186d86374a083755319860f464ea8c9ef1
Zolbayar Batbuyan <23LP5833@G302-CC0093.local>
2024-03-28 09:46:51 +0800

    back

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,18 @@
-
 import Image from "next/image";
 import { StickyImageLayout } from "@/components/StickyImage/LayOut";
+import { Input } from "@/components/Input";
 export default function Home() {
   return (
     <div className="">
+      <div className="relative z-30 ">
+        <Input />
+      </div>
       <div className="w-screen h-[3000px] relative z-30 bg-white"></div>
       <div>
         <StickyImageLayout />
         <div className="w-screen h-[1300px]  "></div>
       </div>
       <div className="w-screen  h-[3000px] z-30 flex bg-slate-500 relative"></div>
-
     </div>
   );
 }
