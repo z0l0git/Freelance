53d33cfe48d485b22a7e82efdc019456422d8edd
Zolbayar <zolbayar.in@gmail.com>
2024-04-02 08:17:11 +0800

    sg

--- frontend/src/components/HeaderSearch/HeaderSearch.tsx
+++ frontend/src/components/HeaderSearch/HeaderSearch.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import { BlueButton } from "../Button";
+import { IoSearchOutline } from "react-icons/io5";
+
 export const HeaderSearch = () => {
   return (
-    <div className="flex justify-between bg-black">
-      <div className="flex">
+    <div className="flex justify-around items-center py-[24px]">
+      <div className="flex items-center gap-[24px]">
         <Image src="/logo.png" alt="Google" width={186} height={49} />
         <p>How It Works</p>
         <p>Why Freelancely</p>
       </div>
-      <div>
-        <div></div>
+      <div className="flex items-center gap-[24px]">
+        <div className="flex items-center">
+          <IoSearchOutline />
+          <input type="text" />
+        </div>
         <div>
           <BlueButton buttonName="Post A Project" />
         </div>
