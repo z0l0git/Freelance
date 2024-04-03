7221ee4cdb9a33d9fb369585f110ec833f4cbbc1
Chinzo <chinzorigt003@gmail.com>
2024-04-03 12:48:03 +0800

    chinzo

--- frontend/src/app/Chinzorig/page.tsx
+++ frontend/src/app/Chinzorig/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Contact } from "@/components/Contact/Contact";
 import { ProjectCard } from "@/components/ProjectCard/ProjectCard";
+import { Filter } from "@/components/Filter/Filter";
 
 const categoryAr = [
   "Python",
@@ -11,23 +12,26 @@ const categoryAr = [
 
 const Chinzorig = () => {
   return (
-    <div>
+    <div >
       {/* <Contact /> */}
-      <ProjectCard
-        title={"Та минь ээ надад нэг ийм ажил байна"}
-        price={"тохиролцоно"}
-        description="I am looking for a Python developer гэжийнэ, энэ ажлыг ч**чих хоншоортой аавын хүү
+      <div className="flex w-screen items-start justify-center bg-slate-200 gap-5 pt-32">
+        <Filter jobs={"Local Jobs"} counter={"35"} />
+        <ProjectCard
+          title={"Та минь ээ надад нэг ийм ажил байна"}
+          price={"тохиролцоно"}
+          description="I am looking for a Python developer гэжийнэ, энэ ажлыг ч**чих хоншоортой аавын хүү
         байвал хүрээд ир за. Хөлс мөнгөн дээрээ нэг их татганаад байхгүй
         гол нь гарууд юмаа аятайхан хийнэ шүү. За тэгээд энэ ажлыг
         хийчихвэл дараа нь өч төчнөөөн байгаа шүү. Өөр юу ч бичдгийм билээ
         яая дээ байз за за өөрсдөө болгооно биз. Би одоо юу вэ энд хөгшин
         чавганц шиг напь напь гээд юүхэв заа"
-        category={categoryAr}
-        expire={"Expire:"}
-        location={"Location:"}
-        level={"Level:"}
-        proporsal={"Proporsal:"}
-      />
+          category={categoryAr}
+          expire={"Expire:"}
+          location={"Location:"}
+          level={"Level:"}
+          proporsal={"Proporsal:"}
+        />
+      </div>
     </div>
   );
 };
