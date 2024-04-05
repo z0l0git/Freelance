2622a646f5399db4193c693a20fe4e03ae315f7f
z0l0git <zolbayar.in@gmail.com>
2024-04-05 11:51:17 +0800

    hello

--- frontend/src/app/sign/page.tsx
+++ frontend/src/app/sign/page.tsx
@@ -1,12 +1,12 @@
+import { Footer } from "@/components/Footer/Footer";
+import { HeaderSearch } from "@/components/HeaderSearch/HeaderSearch";
+import { HeaderSign } from "@/components/HeaderSearch/HeaderSign";
+import { Menu } from "@/components/Menu/Menu";
 import { Zoom } from "@/components/Sign/zoom/Zoom";
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <Zoom />
-    </div>
-  );
+  return <Zoom />;
 };
 
 export default page;
