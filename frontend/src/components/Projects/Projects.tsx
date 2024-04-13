0fba96a1a6c49a5f59367ed2df38ffcc3041deab
Sainjargal Gankhuyag <23LP8204@G302-CC0089.local>
2024-04-13 18:14:58 +0800

    successfully taked the post data cllicked

--- frontend/src/components/Projects/Projects.tsx
+++ frontend/src/components/Projects/Projects.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import { Filter } from "../Filter/Filter";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
 import { AiFillPropertySafety } from "react-icons/ai";
+import { MouseEvent } from "react";
+import { useRouter } from "next/navigation";
 
 type SkillType = {
   name: string;
   id: string;
 };
 type PosdtDataType = {
+  _id: string;
   createdBy: {
     firstName: string;
     lastName: string;
@@ -29,22 +32,31 @@ type ProjectsProps = {
 export const Projects = (props: ProjectsProps) => {
   const { AllPost } = props;
 
+  const { push } = useRouter();
+
+  const handlerClick = (event: MouseEvent<HTMLDivElement>) => {
+    const projectId = event.currentTarget.id;
+    console.log(projectId, "projectid");
+    push(`/projectDetail?id=${projectId}`);
+  };
+
   return (
     <div className="flex items-start  justify-center  ">
       <div className="flex flex-col-reverse gap-[30px]">
         {AllPost?.map((el, index) => {
           return (
-            <ProjectCard
-              key={index}
-              title={el.title}
-              price={el.budget.toLocaleString()}
-              description={el.description}
-              flexible={el.flexible}
-              createdby={el.createdBy.firstName + " " + el.createdBy.lastName}
-              category={el.skills?.map((el2, index2) => {
-                return el2.name;
-              })}
-            />
+            <div key={index} id={el._id} onClick={handlerClick}>
+              <ProjectCard
+                title={el.title}
+                price={el.budget.toLocaleString()}
+                description={el.description}
+                flexible={el.flexible}
+                createdby={el.createdBy.firstName + " " + el.createdBy.lastName}
+                category={el.skills?.map((el2, index2) => {
+                  return el2.name;
+                })}
+              />
+            </div>
           );
         })}
       </div>
